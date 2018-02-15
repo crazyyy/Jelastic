88cb3b5dd91cf9669de9221e2ec904e315b69e99
Vitalii A <crazyyy@gmail.com>
2018-02-15 09:12:25 +0200

    mobile ver ready

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -46,3 +46,62 @@ if (typeof jQuery === "undefined") {
   console.log("jQuery " + jQuery.fn.jquery + " has loaded")
 }
 // Place any jQuery/helper plugins in here.
+// set sidebar size on page load and every time when page size changed
+SetWidgetSize();
+$(window).resize(function(event) {
+  /* Act on the event */
+  SetWidgetSize();
+});
+
+$(window).scroll(function() {
+  FixedSidebar();
+});
+
+jQuery(document).ready(function($) {
+  $('.widget--getyourappliance button').on('click', function(e) {
+    if ($(this).hasClass('disabled')) {
+      e.preventDefault();
+      $(this).parent().parent().append('<p class="error">please read and agree Terms and confirm allow using your pesonal data ... </p>')
+    } else {
+      $('.widget--getyourappliance .error').hide('fast');
+    }
+  })
+  $('.widget--getyourappliance input[type=checkbox]').on('click', function(e) {
+    if ($('.checkbox1').is(':checked') && $('.checkbox2').is(':checked')) {
+      $('.widget--getyourappliance button').removeClass('disabled')
+    } else {
+      $('.widget--getyourappliance button').addClass('disabled')
+    }
+  })
+});
+
+
+function FixedSidebar() {
+
+  var windowWidth = $(window).width();
+
+  if (windowWidth > 640) {
+    var elementPosition = $('.sidebar').offset();
+
+    var footerHeight = $('footer').height();
+    var documentHeight = $(document).height();
+    var fixedWidgetHeight = $('.widget-fixed').height();
+
+    var breakpoint = documentHeight - footerHeight - fixedWidgetHeight;
+
+
+    if (($(window).scrollTop() > elementPosition.top) && ($(window).scrollTop() < breakpoint)) {
+      $('.widget--getyourappliance').addClass('widget-fixed');
+    } else {
+      $('.widget--getyourappliance').removeClass('widget-fixed');
+    }
+  }
+
+}
+
+function SetWidgetSize() {
+  var sidebarWidth = $('.sidebar').width();
+  $('.widget').each(function(index, el) {
+    $(this).width(sidebarWidth)
+  });
+}
